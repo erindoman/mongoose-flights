b549cefe9d05a7076b503c11d286c303ff9315d5
erindoman <erin.doman@gmail.com>
2020-12-03 21:50:57 -0600

    edit typo in model

--- models/flight.js
+++ models/flight.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const flightSchema = new Schema ({
     airline: {type: String, enum: ['American', 'Southwest', 'United']}, 
-    airport: {string: String, enum: ['AUS', 'DEN', 'DFW', 'LAX', 'SAN'], default: 'DEN'},
+    airport: {type: String, enum: ['AUS', 'DEN', 'DFW', 'LAX', 'SAN'], default: 'DEN'},
     flightNo: {type: Number, require: true, min: 10, max: 9999},
     departs: {type: Date}
 }, {
