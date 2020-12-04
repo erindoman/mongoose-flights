937219aaef70393ddcb61dc1fa210aa69cee14cd
erindoman <erin.doman@gmail.com>
2020-12-04 14:43:52 -0600

    update with mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/flights', {
+mongoose.connect(process.env.DATABSE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
