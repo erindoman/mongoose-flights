56fd0c759ad98d4e59643dc99df880a03bd294c8
erindoman <erin.doman@gmail.com>
2020-12-03 21:48:48 -0600

    update model and server

--- models/flight.js
+++ models/flight.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema ({
-    airline: String, 
-    airport: String,
-    flightNo: Number,
-    departs: Date
+    airline: {type: String, enum: ['American', 'Southwest', 'United']}, 
+    airport: {string: String, enum: ['AUS', 'DEN', 'DFW', 'LAX', 'SAN'], default: 'DEN'},
+    flightNo: {type: Number, require: true, min: 10, max: 9999},
+    departs: {type: Date}
+}, {
+    timestamps: true
 })
+
+module.exports = mongoose.model('Flight', flightSchema)
